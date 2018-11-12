ed814ed6091f8c2dd7a59232fc06c7bb6fe2bd69
Antonio Barros da Silva Neto <absn2@WINDOWS.CIN.UFPE.BR>
2018-11-12 16:49:10 -0300

    Parte v1 Classe Pessoas

---
+++ projetoip/pessoas/src/pessoas/RepositorioContas.java
@@ -0,0 +1,9 @@
+package pessoas;
+
+public interface RepositorioContas {
+	void inserir(ContaAbstrata conta);
+	ContaAbstrata procurar(String numero);
+	void remover (String numero);
+	void atualizar (ContaAbstrata conta);
+	boolean existe (String numero);
+}
